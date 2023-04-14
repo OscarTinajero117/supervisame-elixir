fb5dd77522c89f035ecc2e4490bba13edc1e95b9
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-14 16:03:00 -0600

    se crea supervisor

--- lib/supervisame.ex
+++ lib/supervisame.ex
@@ -1,18 +1,23 @@
 defmodule Supervisame do
-  @moduledoc """
-  Documentation for `Supervisame`.
-  """
+  use Supervisor
 
-  @doc """
-  Hello world.
+  def start_link(args) do
+    IO.puts("[Supervisame] Iniciando Supervisor")
+    IO.inspect(args)
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+    # Supervisor.start_link([
+    #   {Supervisame.Calculadora, []},
+    #   {Supervisame.Pila, []}
+    # ], strategy: :one_for_one)
+  end
 
-  ## Examples
-
-      iex> Supervisame.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def init(args) do
+    IO.puts("[Supervisame] Llamando a init")
+    IO.inspect(args)
+    childen = [
+      {Supervisame.Calculadora, []},
+      {Supervisame.Pila, []}
+    ]
+    Supervisor.init(childen, strategy: :one_for_one)
   end
 end
